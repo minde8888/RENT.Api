d244c77c87a9cec7bafef651100d11dcb42c7a46
minde8888 <mindebaltru@gmail.com>
2022-08-10 22:01:45 +0200

    update

--- RENT.Data/Migrations/AppDbContextModelSnapshot.cs
+++ RENT.Data/Migrations/AppDbContextModelSnapshot.cs
@@ -18,7 +18,7 @@ namespace RENT.Data.Migrations
 #pragma warning disable 612, 618
             modelBuilder
                 .HasDefaultSchema("Identity")
-                .HasAnnotation("ProductVersion", "7.0.0-preview.6.22329.4")
+                .HasAnnotation("ProductVersion", "7.0.0-preview.7.22376.2")
                 .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
             NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
